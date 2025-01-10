a297d0ede09c5e2f5f4fa7484f8a055a76f5d909
uttamboda <167513333+uttamboda@users.noreply.github.com>
2025-01-10 12:47:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
  const openModalButton = document.getElementById('open-modal');
     const modal = new bootstrap.Modal(document.getElementById('newton-modal'));
 
-    // Trigger the modal when the button is clicked
     openModalButton.addEventListener('click', function () {
       modal.show();
     });
